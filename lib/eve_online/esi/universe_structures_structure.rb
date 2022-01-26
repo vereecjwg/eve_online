301a1f25824b08ee6b127e5c569b26c4c441f5f1
vereecjwg <64608525+vereecjwg@users.noreply.github.com>
2022-01-26 12:28:47 -0500

    Create universe_structures_structure.rb

---
+++ lib/eve_online/esi/universe_structures_structure.rb
@@ -0,0 +1,33 @@
+module EveOnline
+  module ESI
+    class UniverseStructuresStructure < Base
+      API_PATH = "/v1/universe/structures/%<structure_id>s/"
+
+      attr_reader :structure_id
+
+      def initialize(options)
+        super
+        @structure_id = options.fetch(:structure_id)
+      end
+
+      def structures
+       @assets ||=
+          begin
+            output = []
+            response.each do |asset|
+              output << Models::Structure.new(asset)
+            end
+            output
+          end
+      end
+
+      def scope
+        "read_structures"
+      end
+
+      def path
+        format(API_PATH, structure_id: structure_id)
+      end
+    end
+  end
+end
