53c7ef07af963af989700ba2bea173a9330c2475
vereecjwg <64608525+vereecjwg@users.noreply.github.com>
2022-01-26 16:01:27 -0500

    Create region_orders.rb

---
+++ lib/eve_online/esi/region_orders.rb
@@ -0,0 +1,34 @@
+module EveOnline
+  module ESI
+    class MarketStructures < Base
+      API_PATH = "/v1/markets/%<region_id>s/orders/"
+
+      attr_reader :region_id, :page
+
+      def initialize(options)
+        super
+        @region_id = options.fetch(:region_id)
+        @page = options.fetch(:page, 1)
+      end
+
+      def structure_ids
+       @assets ||=
+          begin
+            output = []
+            response.each do |asset|
+              output << Models::MarketOrder.new(asset)
+            end
+            output
+          end
+      end
+
+      def additional_query_params
+        [:page]
+      end
+
+      def path
+        format(API_PATH, region_id: structure_id)
+      end
+    end
+  end
+end
